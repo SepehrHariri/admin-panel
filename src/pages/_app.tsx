89a5e21cbb70df13d649bb32defd63ae1e40559d
SepehrHariri <sepehrhariry@gmail.com>
2024-10-25 19:12:21 +0300

    useTheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 // import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react"
-import { CssBaseline, ThemeProvider } from "@mui/material";
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
+import { CssBaseline, ThemeProvider, useTheme } from "@mui/material";
 import React from "react"
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
 
 const App: React.FC = ({
   Component, pageProps: { session, ...pageProps }
 }) => {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
