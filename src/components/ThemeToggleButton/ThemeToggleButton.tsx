15fba68463bb83ed37d8c5b7a829951000472db1
SepehrHariri <sepehrhariry@gmail.com>
2024-10-28 14:49:46 +0300

    theme

--- src/components/ThemeToggleButton/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton/ThemeToggleButton.tsx
@@ -11,14 +11,21 @@ export type ThemeToggleButtonProps = {
 }
 
 const ThemeToggleButton = (props: ThemeToggleButtonProps) => {
-
     const mobileCheck = useMediaQuery('(min-width: 500px)');
-
-
-
     const { ColorModeContext = React.createContext({ toggleColorMode: () => {} }) } = props;
     const theme = useTheme();
-    
+    const colorMode = React.useContext(ColorModeContext);
+
+    return (
+        <>
+            {mobileCheck && (
+                <Typography>{theme.palette.mode}</Typography>)   
+            }
+            <IconButton sx={{mr: 2}} title={theme.palette.mode + ' mode'} aria-label={theme.palette.mode + ' mode button'} onClick={colorMode.toggleColorMode} color="inherit">
+                {theme.palette.mode === 'dark' ? <Brightness7Icon/> : <Brightness4Icon/>}
+            </IconButton>
+        </>
+    )
 }
 
 export default ThemeToggleButton
