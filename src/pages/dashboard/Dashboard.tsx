9c3a254a12a8dfe70934b0ea4b40f6bbbcedcc85
SepehrHariri <sepehrhariry@gmail.com>
2024-10-31 13:37:00 +0300

    dashboard grid

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,43 +1,25 @@
-import React from 'react'
-import Head from "next/head";
+import React from 'react';
+import {Box, Grid, Paper} from "@mui/material";
+import scss from './Dashboard.module.scss';
 
-function Dashboard() {
-  return (
-    <>
-      <Head>
-        <title>Dashboard</title>
-        <meta charSet="utf-8" />
-        <meta name="description" content="Admin Panel" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-        <link rel="icon" href="/favicon.ico" />
-
-        
-        <link rel="preload" href="../../_next/static/media/a34f9d1faa5f3315-s.p.woff2" as="font" type="font/woff2" />
-        <link rel="stylesheet" href="../../_next/static/css/f2f2f3f3c506350a.css" data-precedence="next" />
-        <link rel="stylesheet" href="../../_next/static/css/628765f20b848f76.css" data-precedence="next" />
-        <link rel="preload" as="script" fetchPriority="low" href="../../_next/static/chunks/webpack-7b6c32b47e7bd913.js" />
-        <script src="../../_next/static/chunks/fd9d1056-e8c9e75f15b07588.js"></script>
-        <script src="../../_next/static/chunks/23-c9e4af59b587caf8.js"></script>
-
-        <script src="../../_next/static/chunks/591-ae367a43fbafdc16.js"></script>
-        <script src="../../_next/static/chunks/22-74ed33ff63420052.js"></script>
-        <script src="../../_next/static/chunks/118-98e27c7c6a5a93a4.js"></script>
-        <script src="../../_next/static/chunks/123-929ccb2db8122d72.js"></script>
-        <script src="../../_next/static/chunks/992-8d6c36e8a552b8e4.js"></script>
-        <script src="../../_next/static/chunks/252-da2ec07173113868.js"></script>
-        <script src="../../_next/static/chunks/app/(dashboard)/dashboard/default/page-151646a5b36d5ce2.js"></script>
-        <script src="../../_next/static/chunks/231-50995bc066c72b9f.js"></script>
-        <script src="../../_next/static/chunks/648-da9b7a5f80e9b51c.js"></script>
-        <script src="../../_next/static/chunks/482-5c4c5145cd1bc6de.js"></script>
-        <script src="../../_next/static/chunks/app/(dashboard)/layout-beb9171d0d1b437b.js"></script>
-        <meta name="next-size-adjust" />
-        <script src="../../_next/static/chunks/polyfills-78c92fac7aa8fdd8.js"></script>
-
-      </Head>
-      <h1>Dashboard</h1>
-    </>
-  )
+const Dashboard = () => {
+    return (
+        <Box>
+            <Grid container gap={2} className={scss.topCardsContainer} >
+                <Grid>
+                    <Paper className={scss.dataCard}>xs=4</Paper>
+                </Grid>
+                <Grid>
+                    <Paper className={scss.dataCard}>xs=4</Paper>
+                </Grid>
+                <Grid>
+                    <Paper className={scss.dataCard}>xs=4</Paper>
+                </Grid>
+            </Grid>
+            <Grid xs={12} marginY={2}>
+                <Paper className={scss.dataCard}>xs=12</Paper>
+            </Grid>
+        </Box>
+    )
 }
-
-export default Dashboard
+export default Dashboard;
